94f175ea5046e3e5d8096cde5f6fbab385d545cc
Jelle Zwart <jelle.zwart@icloud.com>
2020-10-05 10:52:44 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jelle Zwart on 05/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
